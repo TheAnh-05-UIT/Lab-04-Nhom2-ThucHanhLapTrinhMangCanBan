02d7bab36e0e67324813865c72f5ae41a98ec4d4
Pham Phu Quang <24521478@gm.uit.edu.vn>
2025-12-02 21:49:21 +0700

    xong bai 7

--- Bai07-Lab04/Program.cs
+++ Bai07-Lab04/Program.cs
@@ -16,7 +16,7 @@ namespace Bai07_Lab04
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
