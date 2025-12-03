2ae64ecce21adf3d1c26010a15d8e32a30b5427c
iamabighotdog <anhhao006@gmail.com>
2025-12-03 22:08:07 +0700

    Bai 6

--- Bai06-Lab04/Form1.Designer.cs
+++ Bai06-Lab04/Form1.Designer.cs
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.responseBox = new System.Windows.Forms.TextBox();
+            this.userinfoButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // urlBox
+            // 
+            this.urlBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.urlBox.Location = new System.Drawing.Point(152, 26);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(472, 27);
+            this.urlBox.TabIndex = 0;
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameBox.Location = new System.Drawing.Point(152, 71);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(235, 27);
+            this.usernameBox.TabIndex = 1;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordBox.Location = new System.Drawing.Point(152, 124);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(235, 27);
+            this.passwordBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(425, 71);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(199, 27);
+            this.loginButton.TabIndex = 6;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // responseBox
+            // 
+            this.responseBox.Location = new System.Drawing.Point(38, 170);
+            this.responseBox.Multiline = true;
+            this.responseBox.Name = "responseBox";
+            this.responseBox.Size = new System.Drawing.Size(586, 122);
+            this.responseBox.TabIndex = 7;
+            // 
+            // userinfoButton
+            // 
+            this.userinfoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userinfoButton.Location = new System.Drawing.Point(425, 124);
+            this.userinfoButton.Name = "userinfoButton";
+            this.userinfoButton.Size = new System.Drawing.Size(199, 27);
+            this.userinfoButton.TabIndex = 8;
+            this.userinfoButton.Text = "Get user info";
+            this.userinfoButton.UseVisualStyleBackColor = true;
+            this.userinfoButton.Click += new System.EventHandler(this.userinfoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(677, 314);
+            this.Controls.Add(this.userinfoButton);
+            this.Controls.Add(this.responseBox);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.urlBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox responseBox;
+        private System.Windows.Forms.Button userinfoButton;
     }
 }
 
--- Bai06-Lab04/Form1.cs
+++ Bai06-Lab04/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,104 @@ namespace Bai06_Lab04
 {
     public partial class Form1 : Form
     {
+        private string tokenType;
+        private string accessToken;
+
         public Form1()
         {
             InitializeComponent();
+            urlBox.Text = "https://nt106.uitiot.vn/auth/token";
+        }
+
+        private async void loginButton_Click(object sender, EventArgs e)
+        {
+            string url = urlBox.Text.Trim();
+            if (string.IsNullOrEmpty(url))
+            {
+                url = "https://nt106.uitiot.vn/auth/token";
+            }
+
+            string username = usernameBox.Text.Trim();
+            string password = passwordBox.Text;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ Username và Password.");
+                return;
+            }
+
+            using (var client = new HttpClient())
+            {
+                var content = new MultipartFormDataContent
+                {
+                    { new StringContent(username), "username" },
+                    { new StringContent(password), "password" }
+                };
+
+                try
+                {
+                    var response = await client.PostAsync(url, content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var responseObject = JObject.Parse(responseString);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        var detail = responseObject["detail"]?.ToString();
+                        responseBox.Text = "Đăng nhập không thành công.\r\n"
+                                         + "Detail: " + detail;
+
+                        tokenType = null;
+                        accessToken = null;
+                        return;
+                    }
+                    tokenType = responseObject["token_type"]?.ToString();
+                    accessToken = responseObject["access_token"]?.ToString();
+
+                    responseBox.Text = "Đăng nhập thành công!\r\n"
+                                     + $"{tokenType} {accessToken}";
+                }
+                catch (Exception ex)
+                {
+                    responseBox.Text = "Lỗi khi gọi API:\r\n" + ex.Message;
+                }
+            }
+        }
+        private async void userinfoButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tokenType) || string.IsNullOrEmpty(accessToken))
+            {
+                MessageBox.Show("Bạn cần đăng nhập trước.");
+                return;
+            }
+
+            string getUserUrl = "https://nt106.uitiot.vn/api/v1/user/me";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue(tokenType, accessToken);
+
+                try
+                {
+                    var response = await client.GetAsync(getUserUrl);
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        responseBox.Text = "Không lấy được thông tin user.\r\n"
+                                         + "Phản hồi từ server:\r\n"
+                                         + responseString;
+                        return;
+                    }
+
+                    responseBox.Text = "Thông tin user hiện tại:\r\n"
+                                     + responseString;
+
+                }
+                catch (Exception ex)
+                {
+                    responseBox.Text = "Lỗi khi gọi API (Bài 6):\r\n" + ex.Message;
+                }
+            }
         }
     }
 }
