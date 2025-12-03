bf46ae3f26088706dc2b8526ee81057d052ebf4e
iamabighotdog <anhhao006@gmail.com>
2025-12-03 09:49:06 +0700

    Bai 5

--- Bai05-Lab04/Form1.cs
+++ Bai05-Lab04/Form1.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Bai05_Lab04
 {
@@ -15,6 +18,51 @@ namespace Bai05_Lab04
         public Form1()
         {
             InitializeComponent();
+            urlBox.Text = "https://nt106.uitiot.vn/auth/token";
+        }
+        private async void loginButton_Click(object sender, EventArgs e)
+        {
+            string url = urlBox.Text.Trim();
+            if (string.IsNullOrEmpty(url))
+            {
+                url = "https://nt106.uitiot.vn/auth/token";
+            }
+            string username = usernameBox.Text.Trim();
+            string password = passwordBox.Text;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ Username và Password.");
+                return;
+            }
+            using (var client = new HttpClient())
+            {
+                var content = new MultipartFormDataContent
+                {
+                    { new StringContent(username), "username" },
+                    { new StringContent(password), "password" }
+                };
+
+                try
+                {
+                    var response = await client.PostAsync(url, content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var responseObject = JObject.Parse(responseString);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        var detail = responseObject["detail"]?.ToString();
+                        responseBox.Text = "Đăng nhập không thành công.\r\n"
+                                         + "Detail: " + detail;
+                        return;
+                    }
+                    var tokenType = responseObject["token_type"]?.ToString();
+                    var accessToken = responseObject["access_token"]?.ToString();
+                    responseBox.Text = "Đăng nhập thành công!\r\n" + $"{tokenType} {accessToken}";
+                }
+                catch (Exception ex)
+                {
+                    responseBox.Text = "Lỗi khi gọi API:\r\n" + ex.Message;
+                }
+            }
         }
     }
 }
